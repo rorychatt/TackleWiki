ef7ece0edf9acfe7085161cf36148c0f6dd44775
rorychatt <pavel.rinne@gmail.com>
2024-07-29 09:28:35 +0200

    Feat: init

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+﻿const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.worker\.ts$/,
+                use: { loader: 'worker-loader' }
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html'
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: 'public/style.css', to: 'style.css' }
+            ],
+        })
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 8080,
+    }
+};
