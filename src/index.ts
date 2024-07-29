100bc348c21f2099f54308fbc62644059dfeb2fc
rorychatt <pavel.rinne@gmail.com>
2024-07-29 14:47:08 +0200

    Feat: dynamic article loading

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,49 @@
-﻿async function fetchArticles() {
+﻿async function fetchArticle() {
     try {
         const response = await fetch('http://localhost:5090/api/v1/article');
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
-        const articles = await response.json();
-        displayArticles(articles);
+        const article: Article = await response.json();
+        displayArticle(article);
     } catch (error) {
         console.error('There was a problem with the fetch operation:', error);
     }
 }
 
-function displayArticles(articles: any[]) {
+function displayArticle(article: Article) {
     const articleList = document.querySelector('.article-category-articles');
     if (articleList) {
-        articles.forEach(article => {
-            const listItem = document.createElement('li');
-            listItem.classList.add('article-category-articles__list-item');
-            listItem.innerHTML = `
-                <article class="article">
+        const listItem = document.createElement('li');
+        listItem.classList.add('article-category-articles__list-item');
+        listItem.innerHTML = `
+                <li class="article-category-articles__list-item">
+                    <article class="article">
                     <h3 class="article__header">${article.title}</h3>
                     <p class="article__description">${article.content}</p>
                     <a class="article__link" href="#">Read more</a>
                 </article>
+                </li>
             `;
-            articleList.appendChild(listItem);
-        });
+        articleList.appendChild(listItem);
     }
 }
 
-document.addEventListener('DOMContentLoaded', fetchArticles);
+document.addEventListener('DOMContentLoaded', (event) => {
+    event.preventDefault();
+    for (let i = 0; i < 5; i++) {
+        fetchArticle();
+    }
+});
+
+type Article = {
+    id: string;
+    authorName: string;
+    title: string;
+    content: string;
+    createdAt: string;
+    updatedAt: string;
+    comments: [];
+    tags: [];
+    ratings: [];
+}
